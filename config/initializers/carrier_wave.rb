f7d8ace2d1453b8a2b4e02c058cc208abffc36eb
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-06-25 20:15:36 +0200

    Fix Fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,16 @@
 require 'fog/aws'
+if Rails.env.production?
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAIIE5DDMRFJXFAPSQ',
-    aws_secret_access_key: '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',              region:                'eu-central-1'
+    aws_access_key_id: => ENV ['S3_ACCESS_KEY'],
+    aws_secret_access_key: => ENV ['S3_SECRET_KEY'],
+    region:              => ENV ['S3_REGION']
 
   }
-  config.fog_directory  = 'rails-blog-alex'                          # required
+  config.fog_directory  = ENV ['S3_BUCKET']                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   config.storage = :fog
+end
 end
