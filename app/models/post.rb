a1b285fff1965117b9458bf619f8a27d35383fb8
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-06-25 17:43:41 +0200

    Add file size validation test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   mount_uploader :image, AvatarUploader
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
+  validate :image_size_validation
+
+  private
+
+  def image_size_validation
+    errors[:image] << "should be less than 5MB" if image.size > 3.megabytes
+  end
+
 end
