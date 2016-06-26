5c2aef3203c366871723b944a53bc9ae893c1f0d
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-06-26 11:19:13 +0200

    Add static assets to carrierwave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,10 @@ CarrierWave.configure do |config|
     :provider => 'AWS',
     :aws_access_key_id => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-  
+
   }
-  config.fog_directory  = ENV['S3_BUCKET']                          # required
+  config.fog_directory  = ENV['S3_BUCKET']
+  config.serve_static_assets = true                          # required
 
   end
 end
