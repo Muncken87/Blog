7449580cd4c54885a283760fe5d01dcf752eced9
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-06-26 18:56:14 +0200

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -29,6 +29,6 @@ CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
   config.fog_directory    = ENV['S3_BUCKET']
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+                        # Generate http:// urls. Defaults to :authenticated_read (https://)
 
 end
