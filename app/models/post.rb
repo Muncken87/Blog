3939c5a64d525b09c1f84262800dcd8c87e5922e
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-07-03 21:24:08 +0200

    Fix slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
   extend FriendlyId
-  friendly_id :title, use: [:slugged, :finders]
+  friendly_id :title, use: :slugged
 end
