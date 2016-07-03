e205861af0797c3a248f80f8256531684d9df5f7
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-07-03 22:02:24 +0200

    Add friendly id to delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
 
 
   def destroy
-		@post = Post.find(params[:id])
+		@post = Post.friendly.find(params[:id])
 		@post.destroy
 
 		redirect_to posts_path
