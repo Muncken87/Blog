cb545800803964e4b52a5235e146d1792a743a9b
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-07-04 22:01:49 +0200

    Add user name to devise login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703191137) do
+ActiveRecord::Schema.define(version: 20160704193345) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160703191137) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.boolean  "admin"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
