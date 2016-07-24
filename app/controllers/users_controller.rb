526a2498abbf08690d2379dbe1db283b9a6e71d6
Alexander <yumako87@gmail.com>
2016-07-24 18:56:37 +0200

    Update user ability and style forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:new, :index, :show, :create, :sign_in]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   # GET /users
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-   user ||= User.new # guest user (not logged in)
-   can :manage, User, id: user.id
-   if user.admin?
-    can :manage, :all
-  else
-    can :read, :all
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+     can :manage, :all
+   else
+     can :manage, User, id: user.id
+     can [:index, :show], Posts
+     cannot [:create], Users
+     cannot [:create], Posts
+   end
   end
- end
 end
